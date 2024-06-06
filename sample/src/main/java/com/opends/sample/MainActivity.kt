4fc3989c2f9d7e3e3bf9cd9a47c52b34c1e549f1
Carol Branbila <carolbranbila@hotmail.com>
2024-06-06 14:47:00 +0100

    Create base of home screen

--- sample/src/main/java/com/opends/sample/MainActivity.kt
+++ sample/src/main/java/com/opends/sample/MainActivity.kt
@@ -1,80 +1,73 @@
 package com.opends.sample
 
-import android.content.res.Configuration
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.open.design.system.OpenDesignSystemTheme
+import androidx.compose.ui.unit.dp
 
-@Composable
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-    backgroundColor = 0xffFFFFFF,
-    showBackground = true
-)
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    backgroundColor = 0xff000000,
-    showBackground = true
-)
-fun Test() {
-    Column {
-        Text(
-            color = OpenDesignSystemTheme.color.background,
-            text = "Text working"
-        )
-        Text(
-            color = OpenDesignSystemTheme.color.primary,
-            text = "Text working"
-        )
-        Text(
-            color = OpenDesignSystemTheme.color.secondary,
-            text = "Text working"
-        )
-
-        Box(
-            Modifier
-                .size(OpenDesignSystemTheme.space.lg)
-                .background(OpenDesignSystemTheme.color.primary)
-        ) {
-
-        }
-
-        Box(
-            Modifier
-                .size(OpenDesignSystemTheme.space.md)
-                .background(OpenDesignSystemTheme.color.primary)
-        ) {
-
-        }
-
-        Box(
-            Modifier
-                .size(OpenDesignSystemTheme.space.sm)
-                .background(OpenDesignSystemTheme.color.primary)
-        ) {
-
-        }
-
-        Box(
-            Modifier
-                .size(OpenDesignSystemTheme.space.xl)
-                .background(OpenDesignSystemTheme.color.primary)
-        ) {
-
-        }
-
-        Box(
-            Modifier
-                .size(OpenDesignSystemTheme.space.xs)
-                .background(OpenDesignSystemTheme.color.primary)
-        ) {
-
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Button()
         }
     }
+}
+
+@Composable
+fun Button(
+    modifier: Modifier = Modifier,
+    buttons: List<String> = listOf("Colors", "Spacing", "Typography", "Shadows")
+) {
+    Column(
+        modifier = modifier
+            .fillMaxHeight()
+            .padding(horizontal = 70.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        for (nameButton in buttons) {
+            Buttons(
+                nameButton = nameButton,
+                onContinuedClicked = {}
+            )
+        }
+    }
+}
+
+@Composable
+fun Buttons(
+    modifier: Modifier = Modifier,
+    nameButton: String,
+    onContinuedClicked: () -> Unit,
+) {
+    Button(
+        modifier = modifier
+            .fillMaxWidth(),
+        shape = ShapeDefaults.Large,
+        onClick = onContinuedClicked
+    ) {
+        Text(text = nameButton)
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun ButtonPreview() {
+    Button()
 }
