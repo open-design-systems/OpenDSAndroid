0c821444df6c9a47fdd1b768bd3fea29276fd352
Gabriel Goto <gabrielcosta08@gmail.com>
2024-06-10 20:45:26 +0100

    Update gettokens to return the list

--- plugin/processor/src/main/java/com/opends/processor/WriteThemeAcessor.kt
+++ plugin/processor/src/main/java/com/opends/processor/WriteThemeAcessor.kt
@@ -3,11 +3,17 @@ package com.opends.processor
 import androidx.compose.runtime.Stable
 import com.open.design.system.OpenType
 import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.LIST
 import com.squareup.kotlinpoet.ParameterSpec
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.PropertySpec
+import com.squareup.kotlinpoet.STRING
+import com.squareup.kotlinpoet.TypeName
 import com.squareup.kotlinpoet.TypeSpec
+import com.squareup.kotlinpoet.asClassName
 
 private const val GET_TOKENS_FUNCTION = "gettokens"
 
@@ -48,22 +54,39 @@ fun writeThemeAccessor(
                 .addParameters(mappedParameters)
                 .build()
         )
-        .addFunction(createFunctionToRetrieveTokens(content))
+        .addFunction(createFunctionToRetrieveTokens(content, type))
         .build()
 }
 
 private fun createFunctionToRetrieveTokens(
     content: Set<OpenType>,
+    typeName: TypeName
 ): FunSpec {
-    val getTokens = FunSpec.builder(GET_TOKENS_FUNCTION)
-        .addModifiers(KModifier.INTERNAL)
-        .addStatement("listOf(")
+    val codeBlock = CodeBlock.builder()
 
+    codeBlock.add("return listOf(")
     for (item in content.map { it.meta.name }) {
 
-        getTokens.addStatement("%S to $item,", item)
+        codeBlock.addStatement("%S to $item,", item)
     }
+    codeBlock.add(")")
+    codeBlock.build()
 
-    return getTokens.addStatement(")")
+    val pairType = Pair::class.java.asClassName()
+        .parameterizedBy(
+            STRING,
+            typeName
+        )
+
+    val returnType = LIST
+        .parameterizedBy(pairType)
+
+
+    val getTokens = FunSpec.builder(GET_TOKENS_FUNCTION)
+        .addModifiers(KModifier.INTERNAL)
+        .returns(returnType)
+        .addCode(codeBlock.build())
+
+    return getTokens
         .build()
 }
