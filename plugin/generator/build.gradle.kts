c6d55a3ce2fdc6d8fce60e22a05e9b3891318f86
Gabriel Goto <gabrielcosta08@gmail.com>
2024-10-06 19:52:21 +0100

    Add pom configurations

--- plugin/generator/build.gradle.kts
+++ plugin/generator/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     `kotlin-dsl`
 }
 
+version = properties["POM_VERSION"].toString()
+
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
@@ -12,7 +14,10 @@ gradlePlugin {
     plugins {
         register("opends") {
             id = "com.opends"
+            displayName = "OpenDSAndroid"
+            description = "OpenDSAndroid plugin to create android theme configuration"
             implementationClass = "com.open.design.system.plugin.OpenDesignSystemPlugin"
+            tags.addAll(listOf("android", "theme", "compose-theme", "apps", "mobile"))
         }
     }
 }
@@ -23,4 +28,8 @@ dependencies {
     implementation(project(":processor"))
 
     implementation(libs.plgn.gradle)
+}
+
+apply {
+    from("$rootDir/publish.gradle")
 }
